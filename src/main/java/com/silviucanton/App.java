442207289f75a298e3faf28807fc4387f17c4c04
SilviuCAnton <silviu.c.anton@gmail.com>
2020-03-19 22:29:39 +0200

    Implemented main mock-up test.

--- src/main/java/com/silviucanton/App.java
+++ src/main/java/com/silviucanton/App.java
@@ -1,9 +1,25 @@
 package com.silviucanton;
 
+import com.silviucanton.domain.entities.Athlete;
+import com.silviucanton.domain.exceptions.ValidateException;
+import com.silviucanton.service.AthleteService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");;
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AthleteService athleteService = (AthleteService) context.getBean("athleteServiceImpl");
+        Athlete athlete = new Athlete(22, 0f, "Silviu", "Anton");
+        try {
+            athleteService.saveAthlete(athlete);
+        } catch (ValidateException e) {
+            e.printStackTrace();
+        }
+        System.out.println(athleteService.findAllAthletes());
+        System.out.println(athleteService.getNoAthletes());
+        athleteService.removeAthlete(athlete);
+        System.out.println(athleteService.getNoAthletes());
     }
 
 }
